17147b38023f690edc8825b3448a8dd4f7aeab01
John Todd <jtodd2854@gmail.com>
2020-07-02 14:58:00 -0400

    meets requirements for point degradation

--- Pencil/Pencil.cs
+++ Pencil/Pencil.cs
@@ -1,10 +1,50 @@
+using System.Text;
+
 namespace Pencil
 {
     public class Pencil
     {
+        public static readonly int DEFAULT_DURABILITY = 40_000;
+        public int durability { private set; get; }
+
+        public Pencil() : this(DEFAULT_DURABILITY) {}
+
+        public Pencil(int durability)
+        {
+            this.durability = durability;
+        }
+
+        // NOTE: I am assuming only ASCII characters
         public void write(Paper paper, string text)
         {
-            paper.write(text);
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < text.Length; ++i)
+            {
+                if (text[i] == ' ' || text[i] == '\t' || text[i] == '\n')
+                {
+                    sb.Append(text[i]);
+                }
+                else if (text[i] >= 'A' && text[i] <= 'Z')
+                {
+                    if (durability > 1)
+                    {
+                        durability = durability - 2;
+                        sb.Append(text[i]);
+                    } else {
+                        sb.Append(' ');
+                    }
+                } else
+                {
+                    if (durability > 0)
+                    {
+                        durability = durability - 1;
+                        sb.Append(text[i]);
+                    } else {
+                        sb.Append(' ');
+                    }
+                }
+            }
+            paper.write(sb.ToString());
         }
     }
 }
--- PencilTest/PencilTest.cs
+++ PencilTest/PencilTest.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using NUnit.Framework;
 
 namespace Pencil
@@ -5,6 +6,23 @@ namespace Pencil
     [TestFixture]
     public class PencilTest
     {
+        [Test]
+        public void pencil_created_with_no_parameters_has_default_durability()
+        {
+            var pencil = new Pencil();
+
+            Assert.AreEqual(Pencil.DEFAULT_DURABILITY, pencil.durability);
+        }
+
+        [Test]
+        public void pencil_created_with_durability_has_that_durability()
+        {
+            var durability = 42;
+            var pencil = new Pencil(durability);
+
+            Assert.AreEqual(durability, pencil.durability);
+        }
+
         [Test]
         public void can_write_to_paper()
         {
@@ -26,6 +44,48 @@ namespace Pencil
             pencil.write(paper, newText);
 
             Assert.AreEqual(prevText + newText, paper.read());
+        }
+
+        [Test]
+        public void whitespace_does_not_decrease_durability()
+        {
+            string text = $"all lowercase   with{'\n'}whitespace";
+            var wsQuery = from ch in text where ch == ' ' || ch == '\t' || ch == '\n' select ch;
+            var numWhitespaceChars = wsQuery.Count();
+            var paper = new Paper();
+            var pencil = new Pencil();
+
+            pencil.write(paper, text);
+
+            Assert.AreEqual(text, paper.read());
+            Assert.AreEqual(Pencil.DEFAULT_DURABILITY - text.Length + numWhitespaceChars, pencil.durability);
+        }
+
+        [Test]
+        public void lowercase_characters_decrease_durability_by_one()
+        {
+            var text = "text";
+            var paper = new Paper();
+            var pencil = new Pencil(4);
+
+            pencil.write(paper, text);
+
+            Assert.AreEqual(text, paper.read());
+            Assert.AreEqual(0, pencil.durability);
+        }
+
+        [Test]
+        public void uppercase_characters_decrease_durability_by_two()
+        {
+            var text = "Text";
+            var expectedText = "Tex ";
+            var paper = new Paper();
+            var pencil = new Pencil(4);
+
+            pencil.write(paper, text);
+
+            Assert.AreEqual(expectedText, paper.read());
+            Assert.AreEqual(0, pencil.durability);
         }
     }
 }
